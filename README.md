7fc1e86605324dfeb20b9e0df2cb08c907b3a5fb
David Keller <mrcodeswildride@gmail.com>
2020-06-11 21:10:10 +0000

    es6 update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # score-analyzer
 
-This is a score analyzer web app written in HTML, CSS, and JavaScript.
+Score analyzer web app written in HTML, CSS, and JavaScript.
 
 Use this app at [scoreanalyzer.mrcodeswildride.com](https://scoreanalyzer.mrcodeswildride.com/).
 Learn how to make this app at [mrcodeswildride.com](https://www.mrcodeswildride.com/).
--- script.js
+++ script.js
@@ -1,78 +1,75 @@
-var scoreInput = document.getElementById("score");
-var submitButton = document.getElementById("submit");
-var averageScoreButton = document.getElementById("averageScore");
-var highestScoreButton = document.getElementById("highestScore");
-var lowestScoreButton = document.getElementById("lowestScore");
-var scoresDisplay = document.getElementById("scores");
-var resultDisplay = document.getElementById("result");
+let score = document.getElementById(`score`)
+let submitButton = document.getElementById(`submitButton`)
+let averageButton = document.getElementById(`averageButton`)
+let highestButton = document.getElementById(`highestButton`)
+let lowestButton = document.getElementById(`lowestButton`)
+let scoresParagraph = document.getElementById(`scoresParagraph`)
+let resultParagraph = document.getElementById(`resultParagraph`)
 
-var scores = [];
+let scores = []
 
-submitButton.addEventListener("click", submit);
-averageScoreButton.addEventListener("click", showAverageScore);
-highestScoreButton.addEventListener("click", showHighestScore);
-lowestScoreButton.addEventListener("click", showLowestScore);
+submitButton.addEventListener(`click`, submit)
+averageButton.addEventListener(`click`, showAverageScore)
+highestButton.addEventListener(`click`, showHighestScore)
+lowestButton.addEventListener(`click`, showLowestScore)
 
 function submit() {
-    var scoreValue = parseFloat(parseFloat(scoreInput.value.trim()).toFixed(2));
+  let scoreValue = score.value.trim()
 
-    if (!isNaN(scoreValue)) {
-        scores.push(scoreValue);
+  if (scoreValue != `` && !isNaN(scoreValue)) {
+    scoresParagraph.innerHTML = `${scoresParagraph.innerHTML} ${scoreValue}`
+    score.value = ``
 
-        if (scores.length > 1) {
-            scoresDisplay.innerHTML = scoresDisplay.innerHTML + ", ";
-        }
-
-        scoresDisplay.innerHTML = scoresDisplay.innerHTML + scoreValue;
-        scoreInput.value = "";
-    }
+    scores.push(Number(scoreValue))
+  }
 }
 
 function showAverageScore() {
-    if (scores.length == 0) {
-        resultDisplay.innerHTML = "No score was submitted.";
-    }
-    else {
-        var total = 0;
+  if (scores.length == 0) {
+    resultParagraph.innerHTML = `No score was submitted.`
+  }
+  else {
+    let runningTotal = 0
 
-        for (var i = 0; i < scores.length; i++) {
-            total = total + scores[i];
-        }
-
-        resultDisplay.innerHTML = "Average score: " + parseFloat((total / scores.length).toFixed(2));
+    for (let score of scores) {
+      runningTotal = runningTotal + score
     }
+
+    let average = runningTotal / scores.length
+    resultParagraph.innerHTML = `Average score: ${average}`
+  }
 }
 
 function showHighestScore() {
-    if (scores.length == 0) {
-        resultDisplay.innerHTML = "No score was submitted.";
-    }
-    else {
-        var highestScore = scores[0];
+  if (scores.length == 0) {
+    resultParagraph.innerHTML = `No score was submitted.`
+  }
+  else {
+    let highest = scores[0]
 
-        for (var i = 0; i < scores.length; i++) {
-            if (scores[i] > highestScore) {
-                highestScore = scores[i];
-            }
-        }
-
-        resultDisplay.innerHTML = "Highest score: " + highestScore;
+    for (let score of scores) {
+      if (score > highest) {
+        highest = score
+      }
     }
+
+    resultParagraph.innerHTML = `Highest score: ${highest}`
+  }
 }
 
 function showLowestScore() {
-    if (scores.length == 0) {
-        resultDisplay.innerHTML = "No score was submitted.";
-    }
-    else {
-        var lowestScore = scores[0];
+  if (scores.length == 0) {
+    resultParagraph.innerHTML = `No score was submitted.`
+  }
+  else {
+    let lowest = scores[0]
 
-        for (var i = 0; i < scores.length; i++) {
-            if (scores[i] < lowestScore) {
-                lowestScore = scores[i];
-            }
-        }
-
-        resultDisplay.innerHTML = "Lowest score: " + lowestScore;
+    for (let score of scores) {
+      if (score < lowest) {
+        lowest = score
+      }
     }
+
+    resultParagraph.innerHTML = `Lowest score: ${lowest}`
+  }
 }
